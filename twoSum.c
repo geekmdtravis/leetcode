60e20e60d693655e428705cdd1b7dd1896eec7ac
geekmdtravis <travis@geekmd.io>
2021-01-02 21:46:46 -0800

    Two Sum - brute force implementation completed in C

---
+++ twoSum.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "twoSum.h"
+
+int main () {
+    const int target = 10;
+    int nums[NUMS_COUNT] = { 2, 5, 5, 11 };
+    int returnSize;
+
+    int *result = twoSum(nums, NUMS_COUNT, target, &returnSize);
+    printf("[%d, %d]", result[0], result[1]);
+
+    free(result);
+
+    return 0;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *twoSum(int *nums, int numsSize, int target, int *returnSize){
+    *returnSize = 2;
+    int *returnValues = malloc((*returnSize) * sizeof(int));
+
+    for (int i = 0; i < numsSize - 1; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                returnValues[0] = i;
+                returnValues[1] = j;
+            }
+        }
+    }
+    
+    return returnValues;
+}
---
+++ twoSum.h
@@ -0,0 +1,8 @@
+#ifndef TWO_SUM_HEADER_INCLUDED
+#define TWO_SUM_HEADER_INCLUDED
+
+#define NUMS_COUNT 4
+
+int *twoSum(int *nums, int numsSize, int target, int *returnSize);
+
+#endif
