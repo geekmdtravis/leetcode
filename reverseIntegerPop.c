7be9f6a9cc724a1e64ad982da9ba4036fb295a04
geekmdtravis <travis@geekmd.io>
2021-01-05 12:59:30 -0800

    reverse integer solution implemented by stack

--- reverseIntegerPop.c
+++ reverseIntegerPop.c
@@ -5,22 +5,23 @@
 #include "reverseIntegerPop.h"
 
 int main() {
-    printf("Reversed: %d", reverse(-3431));
+    printf("Reversed: %d", reverse(-2143648));
     return 0;
 }
 
-// TODO: implement using a queue, not a stack. Need FIFO structure for this to work.
-// Or, just read directly from the data in the stack in reverse order (hacky/ugly).
 int reverse(int x) {
-    Stack *s = newStack();
-    unsigned short isNegative = x < 0 ? 1 : 0;
+    if (x <= INT_MIN || x >= INT_MAX) return 0;
 
-    for (int tmp = isNegative ? -x : x; tmp != 0; tmp /= 10) push(s, tmp % 10);
+    Stack *s = newStack();
+
+    for (unsigned int i = abs(x); i != 0; i /= 10) push(s, i % 10);
 
     long retVal = 0;
-    while(s->top >= 0) retVal += pop(s) * (int)(pow(10, (double)s->top));
-
-    retVal *= isNegative ? -1 : 1;
+    for (int curr = 0, end = s->top; curr <= end; curr++) 
+        retVal += pop(s) * pow(10, (double)curr);
+    retVal *= x < 0 ? -1 : 1;
+    
+    free(s);
 
     return retVal >= INT_MIN && retVal <= INT_MAX ? (int)retVal : 0;
 }
@@ -28,6 +29,7 @@ int reverse(int x) {
 Stack *newStack() {
     Stack *new = malloc(sizeof(Stack));
     new->top = -1;
+    return new;
 }
 
 void push(Stack *s, unsigned short num) {
@@ -35,9 +37,7 @@ void push(Stack *s, unsigned short num) {
 }
 
 unsigned short pop(Stack *s) {
-    unsigned short popped = s->data[s->top--];
-
-    return popped;
+    return s->data[s->top--];
 }
 
 unsigned short peek(Stack *s) {
